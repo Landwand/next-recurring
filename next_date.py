5d4802fc39c5200be6944050b86c0b38f18c1993
Winston Fong <winston_fong@hotmail.com>
2021-04-21 21:46:19 -0400

    trying to parse CSV file and send values to calculate next-generation-date

--- next_date.py
+++ next_date.py
@@ -11,6 +11,10 @@ FREQUENCY_REGEX = re.compile(r"(?P<magnitude>\d+)?(?P<unit>[ymw])")
 DURATION_UNIT_MAP = {"w": "weeks", "m": "months", "y": "years"}
 
 
+def convert_to_datetime():
+    return
+
+
 def next_generation_date(create_date, last_occurrence_date, frequency):
     today = datetime.now(tz=FRESHBOOKS_TIMEZONE).date()
     if today < create_date:
@@ -43,38 +47,47 @@ def time_delta_from_frequency(frequency):
     return relativedelta(**{duration_unit_string: magnitude})
 
 
-choices = [1, 2, 3]
 
-while True:
-    choice = input('Which set of variables would you like to use? 1, 2, 3? ')
-    try:
-        choice = int(choice)
-        if choice in choices:
-            print('You chose ', choice)
-            break
 
-    except:
-        print('Bad drive!')
 
-if choice == 1:
-    create_date = date(2021, 1, 1)
-    last_occurrence_date = date(2022, 1, 1)
-    frequency = "3m"
-elif choice == 2:
-    create_date = date(2021, 3, 20)
-    last_occurrence_date = date(2021, 3, 20)
-    frequency = "1y"
-elif choice == 3:
-    create_date = date(2021, 1, 1)
-    last_occurrence_date = date(2023, 6, 15)
-    frequency = "5m"
 
-meta_date = (create_date, last_occurrence_date, frequency)
-for i in meta_date:
-    print(i)
 
-calculated_date = next_generation_date(*meta_date)
-print('    Calculated Next Date ===  ', calculated_date)
+# choices = [1, 2, 3]
+#
+# while True:
+#     choice = input('Which set of variables would you like to use? 1, 2, 3? ')
+#     try:
+#         choice = int(choice)
+#         if choice in choices:
+#             print('You chose ', choice)
+#             break
+#
+#     except:
+#         print('Bad drive!')
+
+# if choice == 1:
+#     create_date = date(2021, 1, 1)
+#     last_occurrence_date = date(2022, 1, 1)
+#     frequency = "3m"
+# elif choice == 2:
+#     create_date = date(2021, 3, 20)
+#     last_occurrence_date = date(2021, 3, 20)
+#     frequency = "1y"
+# elif choice == 3:
+#     create_date = date(2021, 1, 1)
+#     last_occurrence_date = date(2023, 6, 15)
+#     frequency = "5m"
+#
+# meta_date = (create_date, last_occurrence_date, frequency)
+# for i in meta_date:
+#     print(i)
+
+# calculated_date = next_generation_date(*meta_date)
+# print('    Calculated Next Date ===  ', calculated_date)
+
+# def calc_next(create_date, last_occurrence_date, frequency):
+#     calculated_date = next_generation_date(*meta_date)
+#     print('calculating date: ', calculated_date)
 
 ''' 
 -- original code to run program---
@@ -82,4 +95,7 @@ calculated_date = next_generation_date(create_date=date(2021, 1, 1),
                                        last_occurrence_date=date(2022, 1, 1),
                                        frequency="3m")
 print(calculated_date)
+
+"that is your howemork # 3, make this runnable and figure out how to use it to predict ‘next invoice date’ "
+
 '''
